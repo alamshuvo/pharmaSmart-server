496adc28d4975bda163ad03ae83fb721e55513dd
iftakhar <iftakharalamshuvo11@gmail.com>
2024-06-03 23:18:12 +0600

    user related api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 // const jwt=require('jsonwebtoken')
 const cors=require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 5000
 
 // middleware
@@ -27,6 +27,62 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // Send a ping to confirm a successful connection
+    const usersCollection=client.db('parmasmartDB').collection('users')
+   
+
+
+    // User related api  collection post or insert
+    app.post("/users",async(req,res)=>{
+        const user=req.body;
+        // insert email if users dosent exist
+        const queary={email:user.email}
+        const exestingUser=await usersCollection.findOne(queary);
+        if (exestingUser) {
+          return res.send({message:"User already exist",insertedId:null})
+        }
+        const result=await usersCollection.insertOne(user);
+        res.send(result)
+    })
+
+
+  app.get("/users",async(req,res)=>{
+    const result =await usersCollection.find().toArray();
+      res.send(result);
+  })
+
+  app.patch("/users/seller/:id",async(req,res)=>{
+    const id=req.params.id;
+    const queary={_id:new ObjectId(id)};
+    const updateddoc={
+      $set:{
+        role:"seller"
+      }
+    }
+    const result =await usersCollection.updateOne(queary,updateddoc);
+    res.send(result)
+  })
+  app.patch("/users/user/:id",async(req,res)=>{
+    const id=req.params.id;
+    const queary={_id:new ObjectId(id)};
+    const updateddoc={
+      $set:{
+        role:"user"
+      }
+    }
+    const result =await usersCollection.updateOne(queary,updateddoc);
+    res.send(result)
+  })
+
+
+
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
